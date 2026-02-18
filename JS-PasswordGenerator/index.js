df7c182567cdd70cb7d53599128f28de59e76a25
Abhay Kumar <123088078+abhxydeploys@users.noreply.github.com>
2026-02-18 21:44:12 +0530

    2nd solo project - password generator

---
+++ JS-PasswordGenerator/index.js
@@ -0,0 +1,133 @@
+const characters = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'~',
+	'`',
+	'!',
+	'@',
+	'#',
+	'$',
+	'%',
+	'^',
+	'&',
+	'*',
+	'(',
+	')',
+	'_',
+	'-',
+	'+',
+	'=',
+	'{',
+	'[',
+	'}',
+	']',
+	',',
+	'|',
+	':',
+	';',
+	'<',
+	'>',
+	'.',
+	'?',
+	'/',
+];
+
+function generatePassword() {
+	let password = '';
+	let randomizedChars = () => Math.floor(Math.random() * characters.length);
+	for (let i = 0; i < 15; i++) {
+		password += characters[randomizedChars()]
+	}
+	return password;
+}
+
+let passEl1 = document.getElementById('pass-1');
+let passEl2 = document.getElementById('pass-2');
+let copyConfirmMsg = document.getElementById('copy-confirmation')
+
+/* let generateBtn = document.getElementById('generateBtn')
+generateBtn.addEventListener('click', () => {
+    passEl1.textContent = generatePassword();
+    passEl2.textContent = generatePassword();
+})
+*/
+
+function updatePasswords() {
+    passEl1.textContent = generatePassword()
+    passEl2.textContent = generatePassword()
+}
+
+// click to copy
+let passwords = document.querySelectorAll('.pwds');
+passwords.forEach(password => password.addEventListener('click', () => {
+    navigator.clipboard.writeText(password.textContent)
+        .then(() => {
+            copyConfirmMsg.style.display = 'inline'
+            setTimeout(() => {
+                copyConfirmMsg.style.display = 'none'
+            }, 2000)
+        })
+        .catch(err => {
+            console.error('Failed to copy text: ', err);
+        });
+}))
